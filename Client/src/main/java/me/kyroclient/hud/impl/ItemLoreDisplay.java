3838c1754ee71218706edefa31342041f28d6da1
Luke Speer <141758940+Kyrotechnic@users.noreply.github.com>
2023-10-16 14:25:22 -0400

    Turned ItemLoreDisplay into a draggable component

---
+++ Client/src/main/java/me/kyroclient/hud/impl/ItemLoreDisplay.java
@@ -0,0 +1,10 @@
+package me.kyroclient.hud.impl;
+
+import me.kyroclient.hud.DraggableComponent;
+
+public class ItemLoreDisplay extends DraggableComponent {
+    public ItemLoreDisplay()
+    {
+        setSize(0, 0);
+    }
+}
--- Client/src/main/java/me/kyroclient/modules/render/LoreDisplay.java
+++ Client/src/main/java/me/kyroclient/modules/render/LoreDisplay.java
@@ -4,6 +4,7 @@ import me.kyroclient.KyroClient;
 import me.kyroclient.events.GuiChatEvent;
 import me.kyroclient.hud.DraggableComponent;
 import me.kyroclient.hud.impl.InventoryHUD;
+import me.kyroclient.hud.impl.ItemLoreDisplay;
 import me.kyroclient.modules.Module;
 import me.kyroclient.settings.BooleanSetting;
 import me.kyroclient.settings.NumberSetting;
@@ -25,15 +26,29 @@ public class LoreDisplay extends Module {
     public NumberSetting scale = new NumberSetting("Scale", 1, 0.5, 2, 0.1);
     public NumberSetting opacity = new NumberSetting("Opacity", 150, 0, 255, 1);
     public BooleanSetting customFont = new BooleanSetting("Custom Font", false);
+
+    public NumberSetting x;
+    public NumberSetting y;
+
+    public ItemLoreDisplay itemLoreDisplay = new ItemLoreDisplay();
+
     public LoreDisplay()
     {
         super("Lore Display", Category.RENDER);
 
+        this.x = new NumberSetting("X1234", 0.0, -100000.0, 100000.0, 1.0E-5, a -> true);
+        this.y = new NumberSetting("Y1234", 0.0, -100000.0, 100000.0, 1.0E-5, a -> true);
+
         addSettings(
+                x,
+                y,
                 scale,
                 opacity,
                 customFont
         );
+
+        itemLoreDisplay.setPosition(this.x.getValue(), this.y.getValue());
+        itemLoreDisplay.setHidden(true);
     }
 
     @Override
@@ -45,13 +60,13 @@ public class LoreDisplay extends Module {
     @SubscribeEvent
     public void onRender(final RenderGameOverlayEvent.Post event) {
         if (this.isToggled() && event.type.equals(RenderGameOverlayEvent.ElementType.HOTBAR) && KyroClient.mc.thePlayer != null) {
-            render();
+            render(itemLoreDisplay.getX(), itemLoreDisplay.getY());
         }
     }
 
-    public void render()
+    public void render(double x, double y)
     {
-        ItemStack item = KyroClient.mc.thePlayer.getHeldItem();
+        /*ItemStack item = KyroClient.mc.thePlayer.getHeldItem();
         if (item == null) return;
 
         List<String> lore = new ArrayList<>();
@@ -80,19 +95,76 @@ public class LoreDisplay extends Module {
 
         GlStateManager.pushMatrix();
         GlStateManager.scale(scale.getValue(), scale.getValue(), scale.getValue());
-        RenderUtils.drawGradientRoundedRect(3, 3, (float) longest + 2, height, 5, new Color(103, 103, 103, (int) opacity.getValue()).getRGB(), KyroClient.clickGui.getColor().getRGB());
-        int y = 5;
+        RenderUtils.drawBorderedRoundedRect(3, 3, (float) longest + 4, height + 2, 5, 2, new Color(103, 103, 103, (int) opacity.getValue()).getRGB(), KyroClient.clickGui.getColor().getRGB());
+        int yy = 5;
         for (String str : lore)
         {
             if (customFont.isEnabled())
             {
-                Fonts.getPrimary().drawStringWithShadow(str, 4, y, Color.white.getRGB());
-                y += Fonts.getPrimary().getHeight();
+                Fonts.getPrimary().drawStringWithShadow(str, 4, yy, Color.white.getRGB());
+                yy += Fonts.getPrimary().getHeight();
             }
             else
             {
-                KyroClient.mc.fontRendererObj.drawString(str, 4, y, Color.white.getRGB());
-                y += 10;
+                KyroClient.mc.fontRendererObj.drawString(str, 4, yy, Color.white.getRGB());
+                yy += 10;
+            }
+
+        }
+        GlStateManager.popMatrix();*/
+
+        ItemStack item = KyroClient.mc.thePlayer.getHeldItem();
+        if (item == null)
+        {
+            itemLoreDisplay.setHidden(true);
+            itemLoreDisplay.setSize(0, 0);
+            return;
+        }
+
+        itemLoreDisplay.setHidden(false);
+
+        List<String> lore = new ArrayList<>();
+
+        lore.add(SkyblockUtils.getDisplayName(item));
+        NBTTagList compound = item.getTagCompound().getCompoundTag("display").getTagList("Lore", 8);
+        double longest = KyroClient.mc.fontRendererObj.getStringWidth(lore.get(0));
+
+        if (customFont.isEnabled())
+            longest = Fonts.getPrimary().getStringWidth(lore.get(0));
+
+        for (int i = 0; i < compound.tagCount(); i++)
+        {
+            String str = compound.get(i).toString();
+            str = str.substring(1, str.length()-1);
+            if (customFont.isEnabled() && Fonts.getPrimary().getStringWidth(str) > longest)
+                longest = Fonts.getPrimary().getStringWidth(str);
+            else if (KyroClient.mc.fontRendererObj.getStringWidth(str) > longest)
+                longest = KyroClient.mc.fontRendererObj.getStringWidth(str);
+            lore.add(str);
+        }
+
+        int height = (lore.size()*10)+4;
+        if (customFont.isEnabled())
+            height = (lore.size()*Fonts.getPrimary().getHeight())+4;
+
+        GlStateManager.pushMatrix();
+        GlStateManager.scale(scale.getValue(), scale.getValue(), scale.getValue());
+
+        itemLoreDisplay.setSize(longest, height);
+
+        RenderUtils.drawBorderedRoundedRect((float) itemLoreDisplay.getX(), (float) itemLoreDisplay.getY(), (float) longest + 3, height + 1, 5, 2, new Color(103, 103, 103, (int) opacity.getValue()).getRGB(), KyroClient.clickGui.getColor().getRGB());
+        int yy = 5;
+        for (String str : lore)
+        {
+            if (customFont.isEnabled())
+            {
+                Fonts.getPrimary().drawStringWithShadow(str, itemLoreDisplay.getX() + 2, yy + itemLoreDisplay.getY(), Color.white.getRGB());
+                yy += Fonts.getPrimary().getHeight();
+            }
+            else
+            {
+                KyroClient.mc.fontRendererObj.drawString(str, (int) (itemLoreDisplay.getX() + 2), (int) (yy + itemLoreDisplay.getY()), Color.white.getRGB());
+                yy += 10;
             }
 
         }
