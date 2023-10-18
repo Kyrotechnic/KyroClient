5c800dc84a4ea0dffcb9c0c62a9e4474a14fc8d2
Luke Speer <141758940+Kyrotechnic@users.noreply.github.com>
2023-10-18 10:55:33 -0400

    Added failsafes

--- Client/src/main/java/me/kyroclient/modules/garden/FarmingMacro.java
+++ Client/src/main/java/me/kyroclient/modules/garden/FarmingMacro.java
@@ -1,6 +1,8 @@
 package me.kyroclient.modules.garden;
 
 import me.kyroclient.KyroClient;
+import me.kyroclient.events.JoinGameEvent;
+import me.kyroclient.events.PacketReceivedEvent;
 import me.kyroclient.modules.Module;
 import me.kyroclient.settings.BooleanSetting;
 import net.minecraft.block.material.Material;
@@ -8,17 +10,50 @@ import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.init.Blocks;
+import net.minecraft.network.play.server.S08PacketPlayerPosLook;
 import net.minecraft.util.BlockPos;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class FarmingMacro extends Module {
     public BooleanSetting defaultDirection = new BooleanSetting("Default left?", true);
+    public BooleanSetting disableOnWorld = new BooleanSetting("Disable on Join", true);
+    public BooleanSetting rotationSafe = new BooleanSetting("Rotation safe", true);
+    public BooleanSetting stopOnGui = new BooleanSetting("Stop on GUI", true);
     public FarmState farmState;
     public int tickCounter;
+    public int failsafeCounter;
     public FarmingMacro()
     {
         super("Farming Macro", Category.GARDEN);
+
+        addSettings(
+                defaultDirection,
+                disableOnWorld,
+                rotationSafe,
+                stopOnGui
+        );
+    }
+
+    @SubscribeEvent
+    public void packet(PacketReceivedEvent e)
+    {
+        if ((rotationSafe.isEnabled()) && e.packet instanceof S08PacketPlayerPosLook)
+        {
+            if (KyroClient.mc.thePlayer.posY < 30)
+            {
+                sendMessage("Congrats, you farmed the entire farm!");
+            }
+            else
+            {
+                sendMessage("STAFF TELEPORT OR ROTATION! STOPPING");
+                setToggled(false);
+                return;
+            }
+
+            failsafeCounter = 100;
+        }
     }
 
     @Override
@@ -30,6 +65,7 @@ public class FarmingMacro extends Module {
             farmState = FarmState.GOINGRIGHT;
 
         tickCounter = 0;
+        failsafeCounter = 0;
 
         updateState(farmState);
     }
@@ -38,10 +74,27 @@ public class FarmingMacro extends Module {
     public void tick(TickEvent.ClientTickEvent e)
     {
         if (!isToggled()) return;
+        if (stopOnGui.isEnabled() && KyroClient.mc.currentScreen != null)
+        {
+            setToggled(false);
+            sendMessage("GUI OPEN! STOPPING");
+            return;
+        }
 
         BlockPos playerPos = new BlockPos(KyroClient.mc.thePlayer.posX, KyroClient.mc.thePlayer.posY, KyroClient.mc.thePlayer.posZ);
         BlockPos shiftLeft = playerPos.add(0, 0, -1);
         BlockPos shiftRight = playerPos.add(0, 0, 1);
+        BlockPos shiftDown = playerPos.add(0, -1, 0);
+
+        failsafeCounter--;
+        IBlockState shiftedDown = KyroClient.mc.theWorld.getBlockState(shiftDown);
+
+        if (failsafeCounter > 0 && shiftedDown.getBlock() == Blocks.bedrock)
+        {
+            sendMessage("FALSE TELEPORT STAFF HERE");
+            setToggled(false);
+            return;
+        }
 
         IBlockState shiftedLeft = KyroClient.mc.theWorld.getBlockState(shiftLeft);
         IBlockState shiftedRight = KyroClient.mc.theWorld.getBlockState(shiftRight);
@@ -122,6 +175,13 @@ public class FarmingMacro extends Module {
         }
     }
 
+    @SubscribeEvent
+    public void onWorldChange(WorldEvent.Load e)
+    {
+        if (disableOnWorld.isEnabled())
+            setToggled(false);
+    }
+
     public void updateBinds(boolean forward, boolean back, boolean left, boolean right, boolean attack)
     {
         KeyBinding.setKeyBindState(KyroClient.mc.gameSettings.keyBindForward.getKeyCode(), forward);
@@ -135,6 +195,8 @@ public class FarmingMacro extends Module {
     public void onDisable()
     {
         tickCounter = 0;
+        failsafeCounter = 0;
+        updateBinds(false, false, false, false, false);
     }
 
     public enum FarmState
