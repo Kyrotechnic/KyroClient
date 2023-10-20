c1c688cf839ecdb5b17ebfe28f0acb7f7a031537
Luke Speer <141758940+Kyrotechnic@users.noreply.github.com>
2023-10-19 20:44:07 -0400

    Fix jacob hopefully

--- Client/src/main/java/me/kyroclient/modules/garden/AntiJacob.java
+++ Client/src/main/java/me/kyroclient/modules/garden/AntiJacob.java
@@ -1,14 +1,19 @@
 package me.kyroclient.modules.garden;
 
 import me.kyroclient.KyroClient;
+import me.kyroclient.events.PacketReceivedEvent;
 import me.kyroclient.modules.Module;
 import me.kyroclient.settings.BooleanSetting;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S02PacketChat;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class AntiJacob extends Module {
     public BooleanSetting disableNuker = new BooleanSetting("Disable Nuker", true);
     public BooleanSetting disableMacro = new BooleanSetting("Disable Macro", true);
     public BooleanSetting autoReenable = new BooleanSetting("Auto Re-enable", true);
     public static AntiJacob instance;
+    public static final String jacobString = "[NPC] Jacob: The Farming Contest is over!";
     public AntiJacob()
     {
         super("Anti Jacob", Module.Category.GARDEN);
@@ -43,5 +48,20 @@ public class AntiJacob extends Module {
 
         if (disableNuker.isEnabled())
             KyroClient.cropNuker.setToggled(true);
+    }
+
+    @SubscribeEvent
+    public void chat(PacketReceivedEvent event)
+    {
+        if (!isToggled()) return;
+        if (event.packet instanceof S02PacketChat)
+        {
+            S02PacketChat packetChat = (S02PacketChat) event.packet;
+
+            if (packetChat.getChatComponent().getUnformattedText().toLowerCase().equalsIgnoreCase(jacobString))
+            {
+                reenable();
+            }
+        }
     }
 }
