b0ed7f2f93097a10ed9646d2a3170caf2fd5a070
Luke Speer <141758940+Kyrotechnic@users.noreply.github.com>
2023-11-18 20:02:12 -0500

    Installer final

--- Instaler/src/main/java/org/example/Main.java
+++ Instaler/src/main/java/org/example/Main.java
@@ -9,12 +9,13 @@ import java.util.List;
 
 public class Main {
     public static List<String> NATIVES = List.of("avutil-ttv-51.dll", "jinput-dx8.dll", "jinput-dx8_64.dll", "jinput-raw.dll", "jinput-raw_64.dll", "jinput-wintab.dll", "libmfxsw64.dll", "libmp3lame-ttv.dll", "lwjgl.dll", "lwjgl64.dll", "OpenAL32.dll", "OpenAL64.dll", "swresample-ttv-0.dll", "twitchsdk.dll");
-    public static final String FORGE_INSTALL_BASE = 
+    public static final String FORGE_INSTALL_BASE = "https://raw.githubusercontent.com/Kyrotechnic/KyroClient/main/assets/";
+    public static String forgeFile;
     public static final String APPDATA = System.getenv("APPDATA");
     public static void main(String[] args) {
         System.out.println("Installing KyroClient forge edition");
 
-        File forgeFile = new File(APPDATA + "\\versions\\k_1.8.9-forge\\");
+        File forgeFile = new File(Main.forgeFile = (APPDATA + "\\versions\\k_1.8.9-forge\\"));
 
         if (!forgeFile.exists())
         {
@@ -22,12 +23,24 @@ public class Main {
             installForgeFiles();
         }
 
-
+        System.out.println("Installed!");
     }
 
     public static void installForgeFiles()
     {
+        install(FORGE_INSTALL_BASE + "k_1.8.9-forge.jar", new File(forgeFile + "k_1.8.9-forge.jar"));
+        System.out.println("Installing Forge.jar");
+        install(FORGE_INSTALL_BASE + "k_1.8.9-forge.json", new File(forgeFile + "k_1.8.9-forge.json"));
+        System.out.println("Installing Forge.json");
 
+        String assets = forgeFile + "assets/";
+        new File(assets).mkdirs();
+
+        for (String str : NATIVES)
+        {
+            install(FORGE_INSTALL_BASE + "assets/" + str, new File(assets + str));
+            System.out.println("Installing " + str);
+        }
     }
 
     public static boolean install(String url, File install)
